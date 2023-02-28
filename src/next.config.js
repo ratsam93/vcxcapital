49636c4978160e898fb29f512938a7f424116a75
ratsam93 <ratsam93@gmail.com>
2023-03-01 01:37:04 +0530

    Update next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites:[{"source":"*","destination":"https://vxdefilabs.wpcomstaging.com/"}],
 }
 
 module.exports = nextConfig
